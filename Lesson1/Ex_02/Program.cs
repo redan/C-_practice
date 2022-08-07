ba93b0bf705872c2f63873a2e1c283c626c5e179
Daniil S <rewasors@gmail.com>
2022-08-07 14:11:22 +0300

    complete ex02

---
+++ Lesson1/Ex_02/Program.cs
@@ -0,0 +1,10 @@
+﻿
+Console.WriteLine("Програма для сравнения чисел: вы напишите два числа а мы скажем какое больше");
+Console.WriteLine("Напишите первое число:");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > b) {
+    Console.WriteLine($"Число {a} больше");
+} else {
+    Console.WriteLine($"Число {b} больше");
+}
