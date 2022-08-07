b7802e7d040e975b78dae72049d95f8ed68cdfa9
Daniil S <rewasors@gmail.com>
2022-08-07 14:38:51 +0300

    complete ex04

--- Lesson1/Ex_02/Program.cs
+++ Lesson1/Ex_02/Program.cs
@@ -1,7 +1,7 @@
-﻿
-Console.WriteLine("Програма для сравнения чисел: вы напишите два числа а мы скажем какое больше");
+﻿Console.WriteLine("Програма для сравнения чисел: вы напишите два числа а мы скажем какое больше");
 Console.WriteLine("Напишите первое число:");
 int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Напишите второе число:");
 int b = Convert.ToInt32(Console.ReadLine());
 if (a > b) {
     Console.WriteLine($"Число {a} больше");
---
+++ Lesson1/Ex_04/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Програма для сравнения чисел: вы напишите три числа а мы скажем какое больше");
+Console.WriteLine("Напишите первое число:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Напишите второе число:");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Напишите третье число:");
+int c = Convert.ToInt32(Console.ReadLine());
+if (a > b && a > c) {
+    Console.WriteLine($"Число {a} больше");
+} else if (b > a && b > c){
+    Console.WriteLine($"Число {b} больше");
+} else {
+    Console.WriteLine($"Число {c} больше");
+}
