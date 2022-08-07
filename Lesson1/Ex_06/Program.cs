e4e17a085b6cdea8b70e590b0796f2f31a12ef0e
Daniil S <rewasors@gmail.com>
2022-08-07 22:43:40 +0300

    complete ex06

---
+++ Lesson1/Ex_06/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Програма для проверки числа на четность");
+Console.WriteLine("Введите число для проверки");
+int a = Convert.ToInt32(Console.ReadLine());
+if(a == 0){
+    Console.WriteLine("Это ноль!");
+} else if (a % 2 == 0 ) {
+    Console.WriteLine($"Число {a}  четное.");
+} else {
+    Console.WriteLine($"Число {a} не четное.");
+}
