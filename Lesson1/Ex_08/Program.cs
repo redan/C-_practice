a2150da746922a56575bca8f543139d42b3d873e
Daniil S <rewasors@gmail.com>
2022-08-09 00:01:22 +0300

    complete ex08

---
+++ Lesson1/Ex_08/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Программа, которая покажет все четные числа до Х");
+Console.WriteLine("Напишите число");
+int x = Convert.ToInt32(Console.ReadLine());
+List<int> nums = new List<int>();
+for (int i = 0; i < x; i++){
+    if (i % 2 == 0 && i != 0) {
+        nums.Add(i);
+    }
+}
+Console.WriteLine($"Ниже все четные числа от 0 до {x}");
+foreach (int num in nums){
+    Console.WriteLine(num);
+}
