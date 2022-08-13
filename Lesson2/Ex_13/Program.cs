39dc7ed73a5bde49afb0e0468d03cb5740dd3dbb
Daniil S <rewasors@gmail.com>
2022-08-13 23:19:41 +0300

    complete h/w

---
+++ Lesson2/Ex_13/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Напишите 3 значное число");
+
+int num = Convert.ToInt32(Console.ReadLine());
+
+var nums = num.ToString().Select(digit => int.Parse(digit.ToString())).ToArray();
+
+if(nums.Length < 3){
+    Console.WriteLine("В вашем числе нет 3 цифры");
+} else {
+    Console.WriteLine(nums[3]);
+}
---
+++ Lesson2/Ex_15/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Напишите нормер дн недели а мы скажем вам выходной ли он");
+
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num == 6 || num == 7) {
+    Console.WriteLine("Да");
+} else {
+    Console.WriteLine("Нет");
+}
