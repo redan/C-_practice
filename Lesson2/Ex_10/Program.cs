87d4f0476e93d55bed86df06703e6709954566ec
Daniil S <rewasors@gmail.com>
2022-08-13 13:58:22 +0300

    create dir/ complete ex_10

---
+++ Lesson2/Ex_10/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Напишите 3-х значное число");
+
+int num = Convert.ToInt32(Console.ReadLine());
+
+var nums = num.ToString().Select(digit => int.Parse(digit.ToString())).ToArray();
+
+
+Console.WriteLine(nums[1]);
