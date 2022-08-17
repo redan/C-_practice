b02b2e49ed37c4bae12f99701e948dbd46119994
Daniil S <rewasors@gmail.com>
2022-08-17 23:40:17 +0300

    complete ex-19

---
+++ Lesson3/Ex_19/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Напишите число");
+
+int num = Convert.ToInt32(Console.ReadLine());
+
+var nums = num.ToString().Select(digit => int.Parse(digit.ToString())).ToArray();
+int[] numsRewers = new int[nums.Length];
+for (int i = 0; i < nums.Length; i++) {
+    numsRewers[i] = nums[nums.Length-i-1];
+}
+
+
+Console.WriteLine(Enumerable.SequenceEqual(nums, numsRewers));
