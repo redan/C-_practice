5a3612d49a01548d131af7e6b22eb704d3a43bd4
Daniil S <rewasors@gmail.com>
2022-08-23 21:21:57 +0300

    complete h/w Less_3

---
+++ Lesson3/Ex_21/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Напишите первую координату (3 цифры через , (символ запятой)");
+
+string num = Console.ReadLine();
+
+var nums = num.Split(',');
+int[] pointA = new int[3];
+for(int i = 0; i < pointA.Length; i++) {
+    pointA[i] = Convert.ToInt32(nums[i]);
+}
+Console.WriteLine("Напишите вторую координату (3 цифры через , (символ запятой)");
+
+string num2 = Console.ReadLine();
+
+var nums2 = num2.Split(',');
+int[] pointB = new int[3];
+for(int i = 0; i < pointB.Length; i++) {
+    pointB[i] = Convert.ToInt32(nums2[i]);
+}
+var res = Math.Sqrt(Math.Pow(pointA[0]-pointB[0], 2)+Math.Pow(pointA[1]-pointB[1], 2)+Math.Pow(pointA[2]-pointB[2], 2));
+
+Console.WriteLine("Результат:");
+Console.WriteLine(Math.Round(res,2));
---
+++ Lesson3/Ex_23/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Напишите число до которого возводить все в квадраты");
+
+int N = Convert.ToInt32(Console.ReadLine());
+
+int[] resArr = new int[N];
+
+for (int i = 0; i < N; i++) {
+    resArr[i] = Convert.ToInt32(Math.Pow(i+1, 3));
+}
+
+foreach (int res in resArr) {
+    Console.Write(res + " ");
+}
