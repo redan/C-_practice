fdedaa7e32f81e4d5b28c04216a213091d8f8722
Dan <rewasors@gmial.com>
2022-08-29 18:28:32 +0300

    complete h/w L6

--- Lesson6/Ex_43/Program.cs
+++ Lesson6/Ex_43/Program.cs
@@ -1,13 +1,15 @@
-﻿y = k1 * x + b1; 
-y = k2 * x + b2;
+﻿Console.WriteLine("Программа, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются вами далее");
+Console.WriteLine("Задайте значение для b1");
+float b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Задайте значение для b2");
+float b2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Задайте значение для k1");
+float k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Задайте значение для k2");
+float k2 = Convert.ToInt32(Console.ReadLine());
 
-0 = k1 * x + b1 - (k2 * x + b2);
-0 = X(k1-k2) + b1-b2;
-k2 * x + b2;
+float x = - (b1 - b2) / (k1 - k2);
+float y = k2 * x + b2;
 
-b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
-
-5* x + 2  -  9 * x + 4   = 5x-9x + 2-4 = 5x - 9x -2 = -4x -2/ 2 = -4x x = 0.5
-
-x =  k1 - k2 + b1 - b2 
-y = k2 * x + b2
+Console.WriteLine("Ответ");
+Console.WriteLine(x + ";" + y);
